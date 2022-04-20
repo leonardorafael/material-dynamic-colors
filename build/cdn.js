b9fbbb2a3319ea949fe5537e6925ee7e8de25f1f
Leonardo Rafael Wehrmeister <>
2022-04-20 10:15:17 -0300

    Removing babel, using the platform instead

---
+++ build/cdn.js
@@ -0,0 +1,13 @@
+import { minify } from "terser";
+import fs from "fs";
+
+(async() => {
+  let options = { 
+    compress: {
+      drop_console: true
+    }
+  };
+  
+  let output = await minify(fs.readFileSync("src/material-dynamic-colors.js", "utf-8"), options);
+  fs.writeFileSync("dist/cdn/material-dynamic-colors.min.js", output.code, ()=>{});
+})();
