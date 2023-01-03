52fc9badd0fcf06549cd483942cb34caead54993
Leonardo Rafael Wehrmeister <>
2023-01-03 11:48:56 -0300

    Importing as module

--- build/cdn.js
+++ build/cdn.js
@@ -8,7 +8,7 @@ import fs from "fs";
     }
   };
   
-  let output = fs.readFileSync("src/cdn/material-dynamic-colors.js", "utf-8").replace("export default ", "");
+  let output = fs.readFileSync("src/cdn/material-dynamic-colors.js", "utf-8");
   let minified = (await minify(output, options)).code;
   fs.writeFileSync("dist/cdn/material-dynamic-colors.min.js", minified);
 })();
