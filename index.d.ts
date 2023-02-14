34d681caa36356bcf11a0e1f85385c48fba28ae5
Leonardo Rafael Wehrmeister <>
2023-02-14 12:33:43 -0300

    Declaring more explicit

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,37 @@
+interface IMode {  
+  primary: string,
+  onPrimary: string,
+  primaryContainer: string,
+  onPrimaryContainer: string,
+  secondary: string,
+  onSecondary: string,
+  secondaryContainer: string,
+  onSecondaryContainer: string,
+  tertiary: string,
+  onTertiary: string,
+  tertiaryContainer: string,
+  onTertiaryContainer: string,
+  error: string,
+  errorContainer: string,
+  onError: string,
+  onErrorContainer: string,
+  background: string,
+  onBackground: string,
+  surface: string,
+  onSurface: string,
+  surfaceVariant: string,
+  onSurfaceVariant: string,
+  outline: string,
+  inverseOnSurface: string,
+  inverseSurface: string,
+  inversePrimary: string,
+  shadow: string
+}
+
+interface ITheme {
+  light: IMode,
+  dark: IMode
+}
+
 declare module "material-dynamic-colors";
-declare let materialDynamicColors: Function;
+declare function materialDynamicColors(from: string | File | Blob | Event): Promise<ITheme>;
